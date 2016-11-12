97c5b618a158f8d8c1e9f4f0bc9cd3fbefe6eabf
sujit maharjan <mail@sujit.email.np>
2016-11-12 19:22:26 +0545

    app version 1

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 var app = angular.module('labapp',[]);
 
-app.controller('labListController', [ '$http',
-                                      function ($http) {
-                                          var data = this;
-                                          data.displayLab = 1;
-                                          data.labs = [];
-                                          data.filename = "";
-                                          $http.get('labList.json').success(function(response) {
-                                              data.labs = response;
-                                          });
-                                          data.labfileDisplay = function(labno)
-                                          {
-                                              data.displayLab = labno;
-                                          };
-                                          data.codeDisplay = function(src)
-                                          {
-                                              data.filename = src;
-                                          };
-                                          
-                                          
-                  }                                  ]
+app.controller('labListController', function ($scope, $http) {
+    $scope.data = [];
+    var data = $scope.data;              
+    function reset()
+    {
+        $scope.data.displayLab = 0;
+        $scope.data.filename = "";
+    }
+    $http.get('api.json').success(function(response) {
+        $scope.data = response;
+        reset();
+        console.log($scope.data);
+        console.log(data);
+    });
+    
+    $scope.labfileDisplay = function(labno)
+    {
+        $scope.data.displayLab = labno;
+    };
+    $scope.codeDisplay = function(src)
+    {
+        $scope.data.filename = src;
+    };
+    
+}
+                                    
               );
 
                           
